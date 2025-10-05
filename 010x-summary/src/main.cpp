a64251e58ab09669a5c0ceec54a10818ec56eee7
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-10-05 20:03:38 -0300

    Solve question 3

--- 010x-summary/src/main.cpp
+++ 010x-summary/src/main.cpp
@@ -2,7 +2,16 @@
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Enter an integer: ";
+    int num1{ };
+    std::cin >> num1;
+
+    std::cout << "Enter another integer: ";
+    int num2{ };
+    std::cin >> num2;
+
+    std::cout << num1 << " + " << num2 << " is " << num1 + num2 << ".\n";
+    std::cout << num1 << " - " << num2 << " is " << num1 - num2 << ".\n";
 
     return 0;
 }
