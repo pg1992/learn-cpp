fea896ea6cdc4424cb0e3fecb976c6d58cdf6c0d
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-10-05 19:42:39 -0300

    Find the "best" solution for the quiz

--- 0111-first_program/src/main.cpp
+++ 0111-first_program/src/main.cpp
@@ -7,7 +7,8 @@ int main()
     int num{ };
     std::cin >> num;
 
-    std::cout << "Double that number is: " << num * 2 << '\n';
+    std::cout << "Double " << num << " is: " << num * 2 << '\n';
+    std::cout << "Triple " << num << " is: " << num * 3 << '\n';
 
     return 0;
 }
