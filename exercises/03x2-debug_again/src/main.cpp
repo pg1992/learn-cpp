13a8467b7f3a45e4ef7effe250fe01b3ef5085c0
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-11-14 22:31:11 -0300

    Solve section 3.x quiz 2

--- exercises/03x2-debug_again/src/main.cpp
+++ exercises/03x2-debug_again/src/main.cpp
@@ -15,10 +15,8 @@ void writeAnswer(int x)
 
 int main()
 {
-	int x{ };
-	int y{ };
-	x = readNumber();
-	x = readNumber();
+	int x{ readNumber() };
+	int y{ readNumber() };
 	writeAnswer(x/y);
 
 	return 0;
