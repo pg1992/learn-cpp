46483d7cb275e73b50dfc1d8f57ac1af2b057520
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-11-21 19:50:20 -0300

    Check if types are IEC 559/IEEE 754 compliant

---
+++ exercises/0408-is_iec559/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    std::cout << std::boolalpha;
+    std::cout << "float: " << std::numeric_limits<float>::is_iec559 << '\n';
+    std::cout << "double: " << std::numeric_limits<double>::is_iec559 << '\n';
+    std::cout << "long double: " << std::numeric_limits<long double>::is_iec559 << '\n';
+
+    return 0;
+}
