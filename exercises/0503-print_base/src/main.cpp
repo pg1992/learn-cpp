4e76e4189355982e2b7106076d72f89bda9731f2
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-11-24 18:17:14 -0300

    Output values in binary using `std::bitset`

--- exercises/0503-print_base/src/main.cpp
+++ exercises/0503-print_base/src/main.cpp
@@ -1,3 +1,4 @@
+#include <bitset>   // for std::bitset
 #include <iostream>
 
 int main()
@@ -9,6 +10,14 @@ int main()
     std::cout << std::oct << x << '\n';     // octal
     std::cout << std::dec << x << '\n';     // return to decimal
     std::cout << x << '\n';                 // decimal
+
+
+    // std::bitset<8> means we want to store 8 bits
+    std::bitset<8> bin1 { 0b1100'0101 };    // binary literal for binary 1100 0101
+    std::bitset<8> bin2 { 0xC5 };           // hexadecimal literal for binary 1100 0101
+
+    std::cout << bin1 << '\n' << bin2 << '\n';
+    std::cout << std::bitset<4>{ 0b1010 } << '\n';
 
     return 0;
 }
