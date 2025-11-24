8a24deb7242e929c5eab0751b788596e58961d2b
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-11-24 18:23:56 -0300

    Reorder example

--- exercises/0503-print_base/src/main.cpp
+++ exercises/0503-print_base/src/main.cpp
@@ -1,7 +1,9 @@
+#include <format>   // C++20
 #include <bitset>   // for std::bitset
 #include <iostream>
+#include <print>    // C++23
 
-int main()
+void printBase()
 {
     int x { 12 };
     std::cout << x << '\n';                 // decimal (by default)
@@ -10,14 +12,22 @@ int main()
     std::cout << std::oct << x << '\n';     // octal
     std::cout << std::dec << x << '\n';     // return to decimal
     std::cout << x << '\n';                 // decimal
+}
 
-
+void printBitset()
+{
     // std::bitset<8> means we want to store 8 bits
     std::bitset<8> bin1 { 0b1100'0101 };    // binary literal for binary 1100 0101
     std::bitset<8> bin2 { 0xC5 };           // hexadecimal literal for binary 1100 0101
 
     std::cout << bin1 << '\n' << bin2 << '\n';
     std::cout << std::bitset<4>{ 0b1010 } << '\n';
+}
+
+int main()
+{
+    printBase();
+    printBitset();
 
     return 0;
 }
